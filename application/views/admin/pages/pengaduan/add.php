0af25d91da9455b88285c1b04a0b2433c9ce7686
viviwlndri12 <vivi.1212@students.amikom.ac.id>
2021-10-13 15:42:05 +0700

    UPDATE

--- application/views/admin/pages/pengaduan/add.php
+++ application/views/admin/pages/pengaduan/add.php
@@ -15,14 +15,22 @@
           <div class="card-body">
             <h4 class="card-title">Basic form elements</h4>
             <p class="card-description">Basic form elements</p>
-            <form class="forms-sample">
+            <form class="forms-sample" action="proses" method="post">
               <div class="form-group">
-                <label for="exampleInputName1">Nik</label>
-                <input type="text" class="form-control" id="exampleInputName1" placeholder="Name" />
+                <label for="exampleInputName1">Nama user</label>
+                <input type="text" name="nama_user" class="form-control" id="exampleInputName1" placeholder="Name" />
+              </div>
+              <div class="form-user">
+              <select class="form-control" name="user_id">
+                <option  value="">---Select Category---</option>                    
+                <?php foreach($user as $row) { ?>
+                <option value="<?php echo $row->id_user;?>"><?php echo $row->username;?></option>
+            <?php } ?>
+        </select> 
               </div>
               <div class="form-group">
                 <label for="exampleTextarea1">Pengaduan</label>
-                <textarea class="form-control" id="exampleTextarea1" rows="4"></textarea>
+                <textarea type="text" name="pengaduan" class="form-control" id="exampleTextarea1" rows="4"></textarea>
               </div>
               <button type="submit" class="btn btn-primary mr-2"> Submit </button>
               <button class="btn btn-light">Cancel</button>
